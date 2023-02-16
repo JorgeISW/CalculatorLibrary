f6a6f6bade298249636ac3dfd38409a8cca90b3b
Jorge Salazar <jor.idw.mx@gmail.com>
2023-02-16 16:04:45 -0600

    Adding testing to TypeError in calculator.add.py function

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,8 +1,14 @@
+import pytest
 from CalculatorLibrary import calculator
 
 
 def test_add() -> None:
     assert calculator.add(1, 2) == (1 + 2)
+
+
+def test_add_value_error() -> None:
+    with pytest.raises(TypeError):
+        calculator.add(1, "2")
 
 
 def test_subtract() -> None:
